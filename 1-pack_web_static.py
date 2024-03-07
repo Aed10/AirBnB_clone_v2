ddca4e0e56f262dded61d4850b87900be04edd79
Aed10 <eddyyanyayoub@gmail.com>
2024-03-08 00:58:29 +0100

    Pack web_static folder with exception handling

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-"""Fabric script that generates a .tgz archive from the contents of the web_static folder of your AirBnB Clone repo"""
+"""Fabric script that generates a .tgz archive from the contents of
+the web_static folder of your AirBnB Clone repo"""
 
 from fabric.api import local
 from datetime import datetime
@@ -15,5 +16,5 @@ def do_pack():
         file = "versions/web_static_{}.tgz".format(date)
         local("tar -cvzf {} web_static".format(file))
         return file
-    except:
+    except Exception as e:
         return None
