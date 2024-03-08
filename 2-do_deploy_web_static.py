bcc60fcccd8f01935ec161c2c97a30fdc3ed4bc7
Aed10 <eddyyanyayoub@gmail.com>
2024-03-08 01:06:04 +0100

    Add script for deploying archive to web servers

---
+++ 2-do_deploy_web_static.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""Fabric script that distributes an archive to your web servers,"""
+from fabric.api import env, put, run
+from os.path import exists
+
+
+def do_deploy(archive_path):
+    if not exists(archive_path):
+        return False
+
+    try:
+        # Upload the archive to the /tmp/ directory of the web server
+        put(archive_path, "/tmp/")
+
+        # Uncompress the archive to the folder:
+        # /data/web_static/releases/<archive filename> on the web server
+        archive_filename = archive_path.split("/")[-1]
+        archive_folder = "/data/web_static/releases/{}".format(
+            archive_filename.split(".")[0]
+        )
+        run("mkdir -p {}".format(archive_folder))
+        run("tar -xzf /tmp/{} -C {}".format(archive_filename, archive_folder))
+        run("rm /tmp/{}".format(archive_filename))
+
+        # Delete the symbolic link /data/web_static/current from the web server
+        run("rm -rf /data/web_static/current")
+
+        # Create a new symbolic link /data/web_static/current on the web server
+        # linked to the new version of your code
+        run("ln -s {} /data/web_static/current".format(archive_folder))
+        return True
+    except Exception as e:
+        return False
