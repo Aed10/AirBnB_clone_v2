24c7f86ac108ae1061af94771e74c213cab17835
Aed10 <eddyyanyayoub@gmail.com>
2024-04-05 00:45:23 +0000

    Refactor file and variable names in do_deploy function

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -19,36 +19,37 @@ def do_deploy(archive_path):
 
     try:
         # Extract the filename without the extension and the path
-        filename = archive_path.split("/")[-1].split(".")[0]
-        remote_path = f"/tmp/{filename}"
+        file = archive_path.split("/")[-1].split(".")[0]
+        remote_path = f"/tmp/{file}"
 
         # Upload the archive to the /tmp/ directory on the server
         put(archive_path, "/tmp/")
 
         # Create the directory where the archive will be unpacked
-        run(f"mkdir -p /data/web_static/releases/{filename}")
+        run(f"mkdir -p /data/web_static/releases/{file}")
 
         # Unpack the archive
-        run(
-            f"tar -xzvf /tmp/{filename}.tgz -C /data/web_static/releases/{filename}/"
-        )
+        run(f"tar -xzvf /tmp/{file}.tgz -C /data/web_static/releases/{file}/")
 
         # Remove the archive from the server's /tmp/ directory
-        run(f"rm /tmp/{filename}.tgz")
+        run(f"rm /tmp/{file}.tgz")
 
         # Move the contenant outside 'web_static' folder
         run(
-            f"mv /data/web_static/releases/{filename}/web_static/* /data/web_static/releases/{filename}/"
+            run(
+                f"mv /data/web_static/releases/{file}/web_static/* "
+                f"/data/web_static/releases/{file}/"
+            )
         )
 
         # Delete web_static folder
-        run("rm -rf /data/web_static/releases/{filename}/web_static")
+        run(f"rm -rf /data/web_static/releases/{file}/web_static")
         # Delete the current symbolic link
         run("rm -f /data/web_static/current")
 
         # Create a new symbolic link
         run(
-            f"ln -s /data/web_static/releases/{filename}/ /data/web_static/current"
+            f"ln -s /data/web_static/releases/{file}/ /data/web_static/current"
         )
 
         return True
