a5a7ba0cab59351d039f6fa818bea93bd0a02192
Aed10 <eddyyanyayoub@gmail.com>
2024-04-07 23:23:11 +0000

    Refactor do_clean function to use local instead of run in 100-clean_web_static.py and delete out-of-date archives in /data/web_static/releases

--- 100-clean_web_static.py
+++ 100-clean_web_static.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
-from fabric.api import env, put, run, local
-from os.path import exists
-from datetime import datetime
-import os
+from fabric.api import *
+
 
 env.hosts = ["18.206.198.24", "54.236.43.182"]
 env.user = "ubuntu"
@@ -15,19 +13,8 @@ def do_clean(number=0):
     Args:
         number (int): The number of archives to keep (0 by default).
     """
-    number = int(number)
-    if number < 0:
-        return
-
-    # Get a list of all files in the versions directory
-    files = local("ls -1t versions", capture=True).split("\n")
-
-    # Remove all but the most recent archives
     if number == 0:
         number = 1
-    for file in files[number:]:
-        local("rm versions/{}".format(file))
-
-    files = run("ls -1t /data/web_static/releases").split("\n")
-    for file in files[number:]:
-        run("rm -rf /data/web_static/releases/{}".format(file))
+    local(f"rm $(ls -C | head -n $( $(ls -1 | wc -l) - {number}))")
+    with cd("/tmp/"):
+        run(f"rm $(ls -C | head -n $( $(ls -1 | wc -l) - {number}))")
