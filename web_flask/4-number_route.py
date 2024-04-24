0a54f9f98c53eca9531ce57b4eedcae2cdacdb2e
Aed10 <eddyyanyayoub@gmail.com>
2024-04-24 22:01:13 +0100

    Add new route for handling number requests in web_flask/4-number_route.py

---
+++ web_flask/4-number_route.py
@@ -0,0 +1,40 @@
+"""Start a Flask web application"""
+
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def hello_hbnb():
+    """Print Hello HBNB!"""
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def hbnb():
+    """Print HBNB"""
+    return "HBNB"
+
+
+@app.route("/c/<text>", strict_slashes=False)
+def c(text):
+    """Print C followed by the value of the text variable"""
+    return "C {}".format(text.replace("_", " "))
+
+
+@app.route("/python/<text>", strict_slashes=False)
+@app.route("/python/", strict_slashes=False)
+def python(text="is cool"):
+    """Print Python followed by the value of the text variable"""
+    return "Python {}".format(text.replace("_", " "))
+
+
+@app.route("/number/<int:n>", strict_slashes=False)
+def number(n):
+    """Print n is a number if n is an integer"""
+    return "{} is a number".format(n)
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
